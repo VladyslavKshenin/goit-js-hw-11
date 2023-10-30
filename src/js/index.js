5742bb8086a40a2b98ec04c7cf8f12ddb86c54c0
VladyslavKshenin <legendary.1bk@gmail.com>
2023-10-30 12:45:00 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -10,8 +10,9 @@ class ApiService {
   constructor() {
     this.searchQuery = '';
     this.currentPage = 1;
+    this.perPage = 40;
   }
-  
+
   async getData() {
     try {
       const response = await axios.get(URL, {
@@ -20,29 +21,30 @@ class ApiService {
           q: this.searchQuery,
           image_type: 'photo',
           orientation: 'horizontal',
-          safeseatch: true,
+          safesearch: true,
           page: this.currentPage,
-          per_page: 40,
-          endOfResults: false,
+          per_page: this.perPage,
         },
       });
+
       this.currentPage += 1;
       return response.data;
     } catch (error) {
       console.log(error.message);
+      throw error;
     }
   }
-  
+
   resetPage() {
     this.currentPage = 1;
   }
-  
+
   get query() {
     return this.searchQuery;
   }
-  
+
   set query(newQuery) {
-    return (this.searchQuery = newQuery);
+    this.searchQuery = newQuery;
   }
 }
 
@@ -62,89 +64,75 @@ const refs = {
 refs.loadMoreBtn.style.display = 'none';
 const apiImages = new ApiService();
 
-const onSubmit = function (e) {
-  apiImages.resetPage();
+const renderCard = function (dataArr) {
+  const searchQuerries = dataArr.map(item => {
+    return `
+      <div class="photo-card">
+        <a class="photo-link" href="${item.largeImageURL}">
+          <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p class="info-item">
+             <b>${item.likes} Likes</b>
+          </p>
+          <p class="info-item">
+             <b>${item.views} Views</b>
+          </p>
+          <p class "info-item">
+            <b>${item.comments} Comments</b>
+          </p>
+          <p class="info-item">
+            <b>${item.downloads} Downloads</b>
+          </p>
+        </div>
+      </div>`;
+  }).join('');
+  refs.gallery.insertAdjacentHTML('beforeend', searchQuerries);
+  lightbox.refresh();
+};
+
+const handleSuccess = function (data) {
+  const searchQueries = data.hits;
+  if (searchQueries.length < apiImages.perPage) {
+    refs.loadMoreBtn.style.display = 'none';
+    if (searchQueries.length === 0) {
+      Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    } else {
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    }
+  } else {
+    refs.loadMoreBtn.style.display = 'block';
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+  }
+  renderCard(searchQueries);
+};
+
+const handleSubmit = async function (e) {
   e.preventDefault();
+  apiImages.resetPage();
   apiImages.query = e.currentTarget.elements.searchQuery.value.trim();
-   if (apiImages.query === '') {
+  if (apiImages.query === '') {
     Notiflix.Notify.info('Please enter your search query!');
     return;
   }
 
   refs.gallery.innerHTML = '';
-
-  apiImages.getData().then(data => {
-    const searchQuerries = data.hits;
-
-    searchQuerries.length === 0 || searchQuerries.length < 40
-      ? (refs.loadMoreBtn.style.display = 'none')
-      : (refs.loadMoreBtn.style.display = 'block');
-
-    renderCard(searchQuerries);
-
-    if (searchQuerries.length === 0) {
-      return Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
-    if (searchQuerries.length < 40) {
-      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-      return Notiflix.Notify.info(
-        "We're sorry, but you've reached the end of search results."
-      );
-    } else {
-      return Notiflix.Notify.success(
-        `Hooray! We found ${data.totalHits} images.`
-      );
-    }
-  });
+  try {
+    const data = await apiImages.getData();
+    handleSuccess(data);
+  } catch (error) {
+    Notiflix.Notify.failure("Failed to load images. Please try again.");
+  }
 };
 
-
-const onLoadMore = function () {
-   apiImages.getData().then(data => {
-    const searchQuerries = data.hits;
-
-    searchQuerries.length === 0 || searchQuerries.length < 40
-      ? (refs.loadMoreBtn.style.display = 'none')
-      : (refs.loadMoreBtn.style.display = 'block');
-
-    renderCard(searchQuerries);
-    if (searchQuerries.length < 40)
-      Notiflix.Notify.info(
-        "We're sorry, but you've reached the end of search results."
-      );
-  });
+const handleLoadMore = async function () {
+  try {
+    const data = await apiImages.getData();
+    handleSuccess(data);
+  } catch (error) {
+    Notiflix.Notify.failure("Failed to load more images. Please try again.");
+  }
 };
 
-const renderCard = function (dataArr) {
-  const searchQuerries = dataArr
-    .map(item => {
-      return `
-      <div class="photo-card">
-        <a class="photo-link" href="${item.largeImageURL}">
-        <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
-        </a>
-          <div class="info">
-            <p class="info-item">
-               <b>${item.likes}Likes</b>
-            </p>
-            <p class="info-item">
-               <b>${item.views}Views</b>
-            </p>
-            <p class="info-item">
-              <b>${item.comments}Comments</b>
-            </p>
-            <p class="info-item">
-              <b>${item.downloads}Downloads</b>
-            </p>
-          </div>
-        </div>`;
-    })
-    .join('');
-  refs.gallery.insertAdjacentHTML('beforeend', searchQuerries);
-  lightbox.refresh();
-};
-
-refs.form.addEventListener('submit', onSubmit);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.form.addEventListener('submit', handleSubmit);
+refs.loadMoreBtn.addEventListener('click', handleLoadMore);
